607f73dd5aa774fbd9790f821854f1e7cbe81703
blakesak24 <blakesakamoto8@gmail.com>
2022-05-02 09:41:38 -0700

    data class

---
+++ app/src/main/java/com/example/finalprojectbasketball/PlayerService.kt
@@ -0,0 +1,7 @@
+package com.example.finalprojectbasketball
+
+data class players(var id : Int, var first_name : String, var last_name: String, var position: String, var height_feet : Int,
+                   var height_inches: Int, var teamId : Int, var conference : String, var divison : String, var teamName : String)
+{
+
+}
--- app/src/main/java/com/example/finalprojectbasketball/Players.kt
+++
@@ -1,7 +0,0 @@
-package com.example.finalprojectbasketball
-
-import retrofit2.http.GET
-
-interface Players {
-    @GET()
-}
